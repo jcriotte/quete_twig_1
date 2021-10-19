09e2de04923dadb49bfe2dc6152557582a07cbe8
RIOTTE <jcriotte69@gmail.com>
2021-10-19 08:48:55 +0200

    first commit

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+// Get a $twig object from this file.
+require_once __DIR__ . '/../config/twig.php';
+
+$products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre'];
+
+echo $twig->render('products.html.twig', ['products' => $products]);
+
+
